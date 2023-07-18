3f7719c9dcc736863cc88719c186c8d001e5c2ac
林志敏 <darrenlin@digitalgd.com.cn>
2023-07-18 17:41:21 +0800

    ops: header component initialization including vite development environment and vitest with react test library

---
+++ packages/header/src/App.test.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+/**
+ * https://www.robinwieruch.de/react-testing-library/
+ * https://builders.travelperk.com/recipes-to-write-better-jest-tests-with-the-react-testing-library-part-1-670aaf3451d1
+ *
+ */
+describe('App', () => {
+  it('renders App component', () => {
+    render(<App />);
+    // screen.debug();
+    // fails
+    // expect(screen.getByText('Search')).toBeInTheDocument();
+
+    // succeeds
+    expect(screen.getByText('Search:')).toBeInTheDocument();
+
+    // succeeds
+    expect(screen.getAllByText(/Search/)).toBeTruthy();
+  });
+});
