1c0ed512c51cfbd2d2c9482130fff336541d6b56
林志敏 <darrenlin@digitalgd.com.cn>
2023-07-25 17:56:03 +0800

    refactor: squenlize mock and jest integration

--- packages/server/models/index.js
+++ packages/server/models/index.js
@@ -1,12 +1,5 @@
-import { Sequelize, DataTypes } from "sequelize";
+
 import { User } from "./User.model.js";
-const sequelize = new Sequelize("trojan", "root", "Ilove1225!", {
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    idle: 30000,
-    acquire: 60000,
-  },
-});
-await User(sequelize);
-export { sequelize };
+export async function init(sequelize){
+  await User(sequelize);
+}
---
+++ packages/server/service/userService.test.js
@@ -0,0 +1,33 @@
+import { getUsers, init, addUser } from "./userService.js"
+import { ssh224 } from "../utils/index.js";
+import SequelizeMock from 'sequelize-mock';
+const dbMock = new SequelizeMock();
+const username = "admin", password = "123456";
+describe("test user service", function () {
+    beforeEach(async () => {
+        await init(dbMock);
+        dbMock.define('User', {
+            username,
+            password: ssh224(username, password)
+        }, {
+            instanceMethods: {
+                getInfo: function () {
+                    return this.get('username') + ' ' + this.get('password');
+                },
+            },
+        });
+        dbMock.models.User.create = dbMock.models.User.upsert;
+        dbMock.models.User.$queryInterface.$useHandler((query, options)=>{
+            console.log(query);
+        })
+    });
+    it('test get users', async () => {
+        const users = await getUsers();
+        // console.log(users);
+        expect(users.length).toBe(1);
+    });
+    it('test add user', async () => {
+        const isInsert = await addUser("admin", "password");
+        expect(isInsert).toBeTruthy();
+    });
+});
