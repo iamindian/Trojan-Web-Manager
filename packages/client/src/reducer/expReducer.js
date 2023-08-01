181ec56fb5b7e2a8264a0ddec40df2fb14d7a772
林志敏 <darrenlin@digitalgd.com.cn>
2023-08-01 16:52:13 +0800

    feat: add loading button for query expiration and update input validation

--- packages/client/src/reducer/expReducer.js
+++ packages/client/src/reducer/expReducer.js
@@ -1,8 +1,12 @@
-const initializtion = { expiration: '--' };
+const initializtion = { expiration: '--', loading: false };
 export default function exReducer(state = initializtion, action) {
   switch (action.type) {
     case "update":
       return action.payload
+    case "loading":
+      return {loading: true}
+    case "loaded":
+      return {loading: false}
     default:
       return state
   }
