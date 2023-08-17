450be3dcb169d128511a4e036ad5214aa0fc2ca9
林志敏 <darrenlin@digitalgd.com.cn>
2023-08-17 19:23:52 +0800

    style: do not show current page nav link in layout

---
+++ packages/client/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+
+function Login(props) {
+    return (<div style={{
+        display: "flex", justifyContent: "center", alignItem: "center", flexWrap: "wrap"
+    }}>
+        Login
+    </div>)
+}
+export function Component() {
+    return Login();
+}
