26f9d02b13a8dca87985fb0d45c292da299b1699
林志敏 <darrenlin@digitalgd.com.cn>
2023-08-30 16:34:38 +0800

    feat: allow user start server with provided location of .env file

--- packages/server/src/bin.js
+++ packages/server/src/bin.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-import "./index.js";
+const { stdin, stdout } = require('process')
+const path = require('path');
+const { spawn } = require('child_process');
+const args = [path.resolve(__dirname, './index.js')];
+process.argv.slice(2).map((arg)=>{
+    args.push(arg);
+})
+const child = spawn('node', args);
+child.stdout.on('data', (data) => {
+    console.log(`${data}`);
+});
+
+
+
+
+
