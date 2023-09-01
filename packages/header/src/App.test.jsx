41b26cdd24455eb131dd352e0d98c97e6467c2d6
iamindian <iamindianbbq33@gmail.com>
2023-09-01 23:04:54 +0800

    docs: add a comment for a file

--- packages/header/src/App.test.jsx
+++ packages/header/src/App.test.jsx
@@ -5,7 +5,7 @@ import App from './App';
 /**
  * https://www.robinwieruch.de/react-testing-library/
  * https://builders.travelperk.com/recipes-to-write-better-jest-tests-with-the-react-testing-library-part-1-670aaf3451d1
- *
+ * https://flexiple.com/react/react-testing-library-cheat-sheet/
  */
 describe('App', () => {
   it('renders App component', () => {
