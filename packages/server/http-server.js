fcd5cc0cdd4c7c420627c2eebe28e516d682100f
iamindian <iamindianbbq33@gmail.com>
2023-09-01 23:05:09 +0800

    refactor: squenlize mock and jest integration

--- packages/server/http-server.js
+++ packages/server/http-server.js
@@ -1,29 +1,39 @@
 import Koa from "koa";
 import Router from "koa-router";
-import userService from "./service/index.js";
+import { getUsers, addUser, getUserExpiration, init as initService } from "./service/index.js";
 import bodyParser from 'koa-body-parser';
+import { Sequelize } from "sequelize";
+import { init as initModels } from "./models/index.js"
 // import https from "https";
+const sequelize = new Sequelize("trojan", "root", "Ilove1225!", {
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    idle: 30000,
+    acquire: 60000,
+  },
+});
+await initModels(sequelize);
+await initService(sequelize);
 const app = new Koa();
 const router = new Router();
 const HOST = "localhost";
 const HTTP_PORT = 8080;
 // const HTTPS_PORT = 443;
-router.get("/users",async (ctx, next) => {
-  ctx.body = await userService.getUsers(ctx.params.username, ctx.params.password);
+router.get("/users", async (ctx, next) => {
+  ctx.body = await getUsers(ctx.params.username, ctx.params.password);
 }).get("/expiration", async (ctx, next) => {
-  ctx.body = await userService.getUserExpiration(ctx.request.query.username, ctx.request.query.password);
+  ctx.body = await getUserExpiration(ctx.request.query.username, ctx.request.query.password);
 }).put("/adduser", async (ctx, next) => {
-  try{
+  try {
     const user = JSON.parse(ctx.request.body);
-    await userService.addUser(user.username, user.password);
-    ctx.body = "done"
-  }catch(e){
+    ctx.body = await addUser(user.username, user.password);
+  } catch (e) {
     console.error(e);
-    ctx.body = "fail"
   }
 });
 app.use(bodyParser());
 app.use(router.routes());
-app.listen(8080,()=>{
+app.listen(8080, () => {
   console.log('Server running on https://localhost:8080');
 });
