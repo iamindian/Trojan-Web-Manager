e02cb511c4c83a81bb07de50b9c008a2a92e871e
iamindian <iamindianbbq33@gmail.com>
2023-09-01 23:05:20 +0800

    feat: add loading button for query expiration and update input validation

--- packages/client/src/pages/App.jsx
+++ packages/client/src/pages/App.jsx
@@ -1,42 +1,79 @@
 /* eslint-disable react/jsx-no-target-blank */
-import {useState} from 'react'
+import { useState } from 'react'
 import './App.css'
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { useSelector, useDispatch } from 'react-redux'
 import { getExpiration } from "../api/userService";
+import LoadingButton from '@mui/lab/LoadingButton';
+import Stack from '@mui/material/Stack';
+import SendIcon from '@mui/icons-material/Send';
 function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState(false);
+  const [passwordError ,setPasswordError] = useState(false);
+  const userRegex = /^[a-zA-Z@_.]+$/;
+  const passwordRegex = /^[0-9]+$/;
+  const userErrorMessage = "只允许输入允许小写a-z,大写A-Z,@,.";
+  const passwordErrorMessage = "只允许输入数字";
   const exReducer = useSelector((state) => state.exReducer)
   const dispatch = useDispatch();
   const update = value => ({
     type: "update",
     payload: value[0]
   })
-  const fetchExpiration = async ()=>{
-     const response = await getExpiration(username, password)
-     dispatch(update(response.data))
+  const loaded = () => ({
+    type: "loaded"
+  })
+  const loading = () => ({
+    type: "loading"
+  })
+  const fetchExpiration = async () => {
+    const response = await getExpiration(username, password)
+    dispatch(loaded());
+    dispatch(update(response.data))
+  }
+  const checkUsername = (e)=>{
+    if(userRegex.test(e.target.value)){
+      setUsernameError(false);
+      setUsername(e.target.value)
+    }else{
+      setUsernameError(true)
+    }
+  }
+  const checkPassword = (e)=>{
+    if(passwordRegex.test(e.target.value)){
+      setPasswordError(false);
+      setPassword(e.target.value)
+    }else{
+      setPasswordError(true);
+    }
   }
   return (
     <div style={{
       display: "flex", justifyContent: "center", alignItem: "center", flexWrap: "wrap"
     }}>
-      <TextField id="standard-username" label="输入用户名" variant="standard" onChange={(e)=>{
-        setUsername(e.target.value)
-      }}/>
+      <Stack spacing={2} direction="row">
+        <TextField helperText={userErrorMessage} error={usernameError} id="standard-username" label="输入用户名" variant="standard" onBlur={checkUsername} />
+        <TextField helperText={passwordErrorMessage} error={passwordError} id="standard-password" label="输入密码" variant="standard" onBlur={checkPassword} />
+        
+      </Stack>
       <div className='break'></div>
-      <TextField style={{ marginTop: "24px" }} id="standard-password" label="输入密码" variant="standard" onChange={(e)=>{
-        setPassword(e.target.value)
-      }}/> 
-      <div className='break'></div>
-      <button style={{ marginTop: "24px" }} onClick={() => {
-        fetchExpiration();
-      }}>query</button>
+      <div>
+      <LoadingButton style={{marginTop: "24px"}} size="small" onClick={() => {
+          if(!username || !password){
+            return;
+          }
+          dispatch(loading());
+          setTimeout(() => { fetchExpiration(); }, 1000)
+        }} variant="contained" loading={exReducer.loading} endIcon={<SendIcon />}>查询</LoadingButton>
+      </div>
+      
       <div className='break'></div>
       <div style={{ marginTop: "24px" }}>
         <Typography sx={{ fontSize: 18 }} color="text.secondary">
-          测试: {exReducer.expiration}
+          有效期: {exReducer.expiration} 天
         </Typography>
       </div>
       {/* <div className='break'></div>
