fc63ce66069212ad375c5357a5bf70a156933db4
iamindian <iamindianbbq33@gmail.com>
2023-09-01 23:05:16 +0800

    feat: query expiration successfully

--- packages/server/models/User.model.js
+++ packages/server/models/User.model.js
@@ -1,10 +1,16 @@
 import { DataTypes } from "sequelize";
 export async function init(sequelize) {
   const model = sequelize.define("User", {
+    id: { type:DataTypes.INTEGER, primaryKey: true },
     username: DataTypes.STRING,
     password: DataTypes.STRING,
+    start: DataTypes.DATE,
+    delta: DataTypes.INTEGER,
+    quota: DataTypes.INTEGER,
+    download: DataTypes.BIGINT(20),
+    upload: DataTypes.BIGINT(20)
   });
-  await model.sync({ force: true });
+  // await model.sync({ force: true });
 }
 
 
