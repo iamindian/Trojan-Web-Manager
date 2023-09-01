27077a4542016fc517da597e4dc7887ee442dc3e
iamindian <iamindianbbq33@gmail.com>
2023-09-01 23:05:02 +0800

    chore: remove redundant console

--- packages/server/utils/index.js
+++ packages/server/utils/index.js
@@ -3,6 +3,5 @@ export function ssh224(username, password) {
     const hash = crypto.createHash('sha224');
     const data = hash.update(`${username}:${password}`, 'utf-8');
     const gen_hash = data.digest('hex');
-    console.log(gen_hash);
     return gen_hash;
 }
